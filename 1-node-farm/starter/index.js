0bd27d1667e0a3ef12f2ff847ec5a60d04cbae7d
Jaguso <jagutzo@gmail.com>
2020-05-28 17:48:42 -0500

    Used multiple callback functions

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -14,12 +14,17 @@ const fs = require('fs');
 
 // Non-blocking asynchronous way
 // OBS: Next code will print to the console 'Will read file!' first and then the data of start.txt after it finish execution 
+fs.readFile('./txt/start.txt', 'utf-8', (err, data) => {
+  console.log(data);
+});
+console.log('Will read file!');
+
+// Next code will get text from multiple files and write it into another file 
 fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
   fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
     console.log(data2);
     fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
       console.log(data3);
-
       fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
         console.log('Your file has been written!');
       });

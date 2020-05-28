b1de5ac6a9416c38afbc7384d4adc7fbc2b78bd2
Jaguso <jagutzo@gmail.com>
2020-05-28 17:35:34 -0500

    Used asyncronous fs.readFile()

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,11 +1,20 @@
 const fs = require('fs');
 
+// Blocking, synchronous way
 // This uses File System module to read from another file
-const textIn = fs.readFileSync('./txt/input.txt', 'utf-8'); 
-console.log(textIn)
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8'); 
+// console.log(textIn)
 
 
-const textOut = `This is what we know about the avocado ${textIn}.\nCreated on ${Date.now()}`;
-//This writes textOut on the first parameter file
-fs.writeFileSync('./txt/output.txt', textOut);
-console.log('File written!')
+// const textOut = `This is what we know about the avocado ${textIn}.\nCreated on ${Date.now()}`;
+// // This writes textOut on the first parameter file
+// fs.writeFileSync('./txt/output.txt', textOut);
+// console.log('File written!')
+
+
+// Non-blocking asynchronous way
+// OBS: Next code will print to the console 'Will read file!' first and then the data of start.txt after it finish execution 
+fs.readFile('./txt/start.txt', 'utf-8', (err, data) => {
+  console.log(data);
+});
+console.log('Will read file!');

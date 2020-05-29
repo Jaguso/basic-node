f9076a726d50ce47b0916a9a0b10cb8d5b6671fd
Jaguso <jagutzo@gmail.com>
2020-05-29 18:50:17 -0500

    Created a server

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,36 +1,51 @@
 const fs = require('fs');
-
-// Blocking, synchronous way
-// This uses File System module to read from another file
-const textIn = fs.readFileSync('./txt/input.txt', 'utf-8'); 
-console.log(textIn)
+const http = require('http');
 
 
-const textOut = `This is what we know about the avocado ${textIn}.\nCreated on ${Date.now()}`;
-// This writes textOut on the first parameter file
-fs.writeFileSync('./txt/output.txt', textOut);
-console.log('File written!')
+
+////////////////////////////
+// FILES
+
+// // Blocking, synchronous way
+// // This uses File System module to read from another file
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8'); 
+// console.log(textIn)
 
 
-// Non-blocking asynchronous way
-// OBS: Next code will print to the console 'Will read file!' first and then the data of start.txt after it finish execution 
-fs.readFile('./txt/start.txt', 'utf-8', (err, data) => {
-  console.log(data);
+// const textOut = `This is what we know about the avocado ${textIn}.\nCreated on ${Date.now()}`;
+// // This writes textOut on the first parameter file
+// fs.writeFileSync('./txt/output.txt', textOut);
+// console.log('File written!')
+
+
+// // Non-blocking asynchronous way
+// // OBS: Next code will print to the console 'Will read file!' first and then the data of start.txt after it finish execution 
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data) => {
+//   console.log(data);
+// });
+// console.log('Will read file!');
+
+// // Next code will get text from multiple files and write it into another file 
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+//   if (err) return console.log('ERROR!');
+
+//   fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//     console.log(data2);
+//     fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+//       console.log(data3);
+//       fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+//         console.log('Your file has been written!');
+//       });
+//     })
+//   });
+// });
+// console.log('Will read file!');
+
+
+
+//////////////////////////////////////
+// SERVER
+
+http.createServer((req, res) => {
+  res.end('Hello from the server!');
 });
-console.log('Will read file!');
-
-// Next code will get text from multiple files and write it into another file 
-fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
-  if (err) return console.log('ERROR!');
-
-  fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
-    console.log(data2);
-    fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
-      console.log(data3);
-      fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
-        console.log('Your file has been written!');
-      });
-    })
-  });
-});
-console.log('Will read file!');

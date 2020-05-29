a6229d7f5fc38feed4bc181cc1873f8da45993fd
Jaguso <jagutzo@gmail.com>
2020-05-29 18:42:54 -0500

    Handled error

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -2,14 +2,14 @@ const fs = require('fs');
 
 // Blocking, synchronous way
 // This uses File System module to read from another file
-// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8'); 
-// console.log(textIn)
+const textIn = fs.readFileSync('./txt/input.txt', 'utf-8'); 
+console.log(textIn)
 
 
-// const textOut = `This is what we know about the avocado ${textIn}.\nCreated on ${Date.now()}`;
-// // This writes textOut on the first parameter file
-// fs.writeFileSync('./txt/output.txt', textOut);
-// console.log('File written!')
+const textOut = `This is what we know about the avocado ${textIn}.\nCreated on ${Date.now()}`;
+// This writes textOut on the first parameter file
+fs.writeFileSync('./txt/output.txt', textOut);
+console.log('File written!')
 
 
 // Non-blocking asynchronous way
@@ -21,6 +21,8 @@ console.log('Will read file!');
 
 // Next code will get text from multiple files and write it into another file 
 fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+  if (err) return console.log('ERROR!');
+
   fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
     console.log(data2);
     fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {

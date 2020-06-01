1ed17710bd261233dc09a972e9de593587c50b2a
Jaguso <jagutzo@gmail.com>
2020-06-01 11:48:34 -0500

    Got query and pathname from req.url

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -71,10 +71,10 @@ const dataObj = JSON.parse(data);
 
 
 const server = http.createServer((req, res) => {
-  const pathName = req.url;
-  
+  const { query, pathname } = url.parse(req.url, true)
+
   // Overview page
-  if (pathName === '/' || pathName === '/overview') {
+  if (pathname === '/' || pathname === '/overview') {
     res.writeHead(200, { 'Content-type': 'text/html' });
 
     const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
@@ -83,11 +83,12 @@ const server = http.createServer((req, res) => {
     res.end(output);
 
   // Product page
-  } else if (pathName === '/product') {
+  } else if (pathname === '/product') {
+    console.log(query)
     res.end('This is the PRODUCT');
 
   // API
-  } else if (pathName === '/api') {
+  } else if (pathname === '/api') {
     fs.readFile(`${__dirname}/dev-data/data.json`, 'utf-8', (err, data) => {
       res.writeHead(200, { 'Content-type': 'application/json' }); // se tiene que poner el tipo de dato que se envía en el header
       res.end(data);

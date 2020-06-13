60dc3ecd3fc363cdf7bf70da391c319a8900e4a1
Jaguso <jagutzo@gmail.com>
2020-06-12 20:39:05 -0500

    Created a server listener

--- 2-how-node-works/starter/events.js
+++ 2-how-node-works/starter/events.js
@@ -1,12 +1,19 @@
 const EventEmitter = require('events');
+const http = require('http');
 
-const myEmitter = new EventEmitter();
+// Two ways of doing the same:
+// 1
+// const myEmitter = new EventEmitter();
 
-// class Sales extends EventEmitter {
-//   constructor() {
-//     super();
-//   }
-// }
+// 2
+class Sales extends EventEmitter {
+  constructor() {
+    super();
+  }
+}
+
+const myEmitter = new Sales();
+////////
 
 
 myEmitter.on('newSale', () => {
@@ -18,7 +25,30 @@ myEmitter.on('newSale', () => {
 });
 
 myEmitter.on('newSale', stock => {
-  console.log(`There are now ${stock} items left in stock.`)
+  console.log(`There are now ${stock} items left in stock`)
 })
 
 myEmitter.emit('newSale', 9);
+
+
+///////////////////////////
+
+const server = http.createServer();
+
+server.on('request', (req, res) => {
+  console.log('Request received')
+  console.log(req.url) // this shows the url of the request
+  res.end('Request received')
+});
+
+server.on('request', (req, res) => {
+  console.log('Another request :)')
+});
+
+server.on('close', () => {
+  console.log('Server closed')
+})
+
+server.listen(8000, 'localhost', () => {
+  console.log('Waiting for requests...')
+})

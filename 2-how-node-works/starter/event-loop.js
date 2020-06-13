75f1f1fd00529b3a986cd9646fd854eb08d434be
Jaguso <jagutzo@gmail.com>
2020-06-12 20:06:14 -0500

    Added event loop examples

--- 2-how-node-works/starter/event-loop.js
+++ 2-how-node-works/starter/event-loop.js
@@ -1,4 +1,10 @@
 const fs = require('fs');
+const crypto = require('crypto');
+
+const start = Date.now();
+
+// This changes the Threadpool size
+process.env.UV_THREADPOOL_SIZE = 3;
 
 setTimeout(() => console.log('Timer 1 finishes'), 0);
 setImmediate(() => console.log('Immediate 1 finished'))
@@ -6,6 +12,55 @@ setImmediate(() => console.log('Immediate 1 finished'))
 
 fs.readFile('test-file.txt', () => {
   console.log('I/O finished');
+  console.log('-----');
+
+  setTimeout(() => console.log('Timer 2 finishes'), 0);
+  setTimeout(() => console.log("Timer 3 finished"), 3000);
+  setImmediate(() => console.log('Immediate 2 finished'));
+
+  process.nextTick(() => console.log('Process.nextTick'));
+
+  // crypto.pbkdf2('password', 'salt', 10000, 1024, 'sha512', () => {
+  //   console.log(Date.now() - start, "Password encrypted")
+  // });
+
+  // crypto.pbkdf2('password', 'salt', 10000, 1024, 'sha512', () => {
+  //   console.log(Date.now() - start, "Password encrypted")
+  // });
+
+  // crypto.pbkdf2('password', 'salt', 10000, 1024, 'sha512', () => {
+  //   console.log(Date.now() - start, "Password encrypted")
+  // });
+
+  // crypto.pbkdf2('password', 'salt', 10000, 1024, 'sha512', () => {
+  //   console.log(Date.now() - start, "Password encrypted")
+  // });
 });
 
 console.log('Hello from the top-level code');
+
+// Next uses pbkdf2Sync the syncronous version
+fs.readFile('test-file.txt', () => {
+  console.log('I/O finished');
+  console.log('-----');
+
+  setTimeout(() => console.log('Timer 2 finishes'), 0);
+  setTimeout(() => console.log("Timer 3 finished"), 3000);
+  setImmediate(() => console.log('Immediate 2 finished'));
+
+  process.nextTick(() => console.log('Process.nextTick'));
+
+  crypto.pbkdf2Sync('password', 'salt', 10000, 1024, 'sha512');
+  console.log(Date.now() - start, "Password encrypted")
+
+  crypto.pbkdf2Sync('password', 'salt', 10000, 1024, 'sha512');
+  console.log(Date.now() - start, "Password encrypted")
+
+  crypto.pbkdf2Sync('password', 'salt', 10000, 1024, 'sha512');
+  console.log(Date.now() - start, "Password encrypted")
+
+  crypto.pbkdf2Sync('password', 'salt', 10000, 1024, 'sha512');
+  console.log(Date.now() - start, "Password encrypted")
+
+});
+

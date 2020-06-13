fc47f4f4bac76ad084121f9837b07725f3544cae
Jaguso <jagutzo@gmail.com>
2020-06-12 20:21:28 -0500

    Worked with events

---
+++ 2-how-node-works/starter/events.js
@@ -0,0 +1,24 @@
+const EventEmitter = require('events');
+
+const myEmitter = new EventEmitter();
+
+// class Sales extends EventEmitter {
+//   constructor() {
+//     super();
+//   }
+// }
+
+
+myEmitter.on('newSale', () => {
+  console.log('There was a new sale!');
+});
+
+myEmitter.on('newSale', () => {
+  console.log('Customer name: Jonas');
+});
+
+myEmitter.on('newSale', stock => {
+  console.log(`There are now ${stock} items left in stock.`)
+})
+
+myEmitter.emit('newSale', 9);

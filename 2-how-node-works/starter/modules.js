c6243d9778a2a4c7f82b95752ccb7d29cdf07429
Jaguso <jagutzo@gmail.com>
2020-08-24 10:43:52 -0500

    Used caching

--- 2-how-node-works/starter/modules.js
+++ 2-how-node-works/starter/modules.js
@@ -7,5 +7,13 @@ const calc1 = new C();
 console.log(calc1.add(2, 5));
 
 // exports
-const calc2 = require('./test-module-2')
-console.log(calc2.add(2, 7));
+// const calc2 = require('./test-module-2')
+const { add, multiply, divide } = require('./test-module-2')
+console.log(add(2, 7));
+console.log(multiply(2, 40));
+
+
+// caching
+require('./test-module-3')();
+require('./test-module-3')();
+require('./test-module-3')();

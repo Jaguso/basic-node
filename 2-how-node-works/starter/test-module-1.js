8ee91de77990721460abfdfbdc2b1a43988951c9
Jaguso <jagutzo@gmail.com>
2020-08-24 10:38:41 -0500

    Second way to export/import

--- 2-how-node-works/starter/test-module-1.js
+++ 2-how-node-works/starter/test-module-1.js
@@ -1,4 +1,26 @@
-class Calculator {
+
+// You can export a class in the two following ways
+// 1
+// class Calculator {
+//   add(a, b) {
+//     return a + b;
+//   }
+
+//   multiply(a, b) {
+//     return a * b;
+//   }
+
+//   divide(a, b) {
+//     return a / b;
+//   }
+// }
+
+// module.exports = Calculator;
+
+
+// 2 
+
+module.exports = class {
   add(a, b) {
     return a + b;
   }
@@ -10,6 +32,4 @@ class Calculator {
   divide(a, b) {
     return a / b;
   }
-}
-
-module.exports = Calculator;
+};

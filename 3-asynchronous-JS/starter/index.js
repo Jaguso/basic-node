10ff39ac0a825e493068b894a4bbf72e7d5e80a3
Jaguso <jagutzo@gmail.com>
2020-08-27 18:16:33 -0500

    Added callbacks

--- 3-asynchronous-JS/starter/index.js
+++ 3-asynchronous-JS/starter/index.js
@@ -7,7 +7,12 @@ fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
   superagent
     .get(`https://dog.ceo/api/breed/${data}/images/random`)
     .end((err, res) => {
+      if (err) return console.log(err.message);
       console.log(res.body.message);
+
+      fs.writeFile('dog-img.txt', res.body.message, err => {
+        console.log('Random dog image sent to file')
+      })
     });
 });
 

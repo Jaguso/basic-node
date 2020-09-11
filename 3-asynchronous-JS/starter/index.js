172f14efd24c9f4a3b496b8ebb7df74a9fea15ab
Jaguso <jagutzo@gmail.com>
2020-09-11 13:59:24 -0500

    Turned code into a flat structure of chained promises

--- 3-asynchronous-JS/starter/index.js
+++ 3-asynchronous-JS/starter/index.js
@@ -10,26 +10,30 @@ const readFilePro = file => {
   })
 }
 
-readFilePro(`${__dirname}/dog.txt`).then(data => {
-  console.log('data', data)
-});
+
+const writeFilePro = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, err => {
+      if (err) reject('Could not write the file')
+      resolve('success');
+    })
+  })
+}
 
 
+readFilePro(`${__dirname}/dog.txt`)
+  .then(data => {
+    console.log(`Breed ${data}`)
+    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`)
+  })
+  .then(res => {
+    console.log(res.body.message)
+    return writeFilePro('dog-img.txt', res.body.message);
+  }).then(() => {
+    console.log('Random dog img saved to file.')
+  })
+  .catch(err => {
+    console.log(err.body.message)
+  })
 
 
-
-fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
-  console.log(`Breed: ${data}`);
-
-  superagent
-    .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .end((err, res) => {
-      if (err) return console.log(err.message);
-      console.log(res.body.message);
-
-      fs.writeFile('dog-img.txt', res.body.message, err => {
-        if (err) return console.log(err.message);
-        console.log('Random dog image sent to file');
-      })
-    });
-});

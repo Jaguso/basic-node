ad32c89690133207ae91d27665113a4250c1b6cb
Jaguso <jagutzo@gmail.com>
2020-10-05 21:27:44 -0500

    b

--- 3-asynchronous-JS/scratch.js
+++ 3-asynchronous-JS/scratch.js
@@ -17,3 +17,16 @@ getNames().then(data => {
 })
 
 callNames()
+
+
+// EXERCISE
+// In this challenge you will receive an array of unresolved promises which each resolve to a number.
+
+// Write a function called promiseResolver which takes the array of unresolved promises as an argument, and returns a promise which resolves to an array containing the resolved values of the original array of unresolved promises(maintaining the order).
+
+// Example pseudo code:
+
+// // example array of unresolved promises which will be passed in as an argument
+// [ Promise { resolve(3) },
+//   Promise { resolve(6) },
+//   Promise { resolve(9) } ]

0e27715c3768b0573a50d7e06dff6248762af56a
Jaguso <jagutzo@gmail.com>
2020-12-14 19:29:35 -0600

    Used then method instead

--- 3-asynchronous-JS/starter/index.js
+++ 3-asynchronous-JS/starter/index.js
@@ -5,16 +5,18 @@ fs.readFile(`${__dirname}/dog.txt`, (er, data) => {
   console.log(`Breed: ${data}`);
 
   superagent
-    .get(`https://dog.ceo/api/breeds/${data}/image/random`)
-    .end((err, res) => {
-      if (err) return console.log(err.message)
+    .get(`https://dog.ceo/api/breed/${data}/images/random`)
+    .then(res => {
       console.log(res.body);
 
       fs.writeFile('dog-img.txt', res.body.message, err => {
         if (err) return console.log(err.message)
         console.log('Random dog image saved to file')
       });
-    });
+    })
+    .catch(err => {
+      console.log(err.message);
+    })
 });
 
 
